f7c7a62a4e12fdaca9b7afe32230958eab514963
TanJeeSchuan <89920999+TanJeeSchuan@users.noreply.github.com>
2023-07-25 19:52:23 +0800

    Initial Commit

---
+++ src/assignmentsource/stationeryPos.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assignmentsource;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Tan Jee Schuan
+ */
+public class stationeryPos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.print("Hello World!");
+    }
+    
+}
